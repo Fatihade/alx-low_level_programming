aacfcf086d9dff86c5d365474a9a1e2697c286c0
Fatihade <adekoyaadeola1234@gmail.com>
2022-09-09 07:45:30 -0700

    Hello,world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	Return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
