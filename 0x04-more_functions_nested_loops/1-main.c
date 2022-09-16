72ece66bc975d6677df930bd0b19bb79cd7fcfb2
Fatihade <adekoyaadeola1234@gmail.com>
2022-09-16 04:05:01 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: 1 if c is a digit, 0 if otherwise.
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c));
+	return (0);
+}
